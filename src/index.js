c093618fcb3475f3ddc6d2d778afa4efa99ae727
Ruslan Yuldashev <rustik.yuldashev@yandex.ru>
2022-06-02 21:05:38 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+// const elem = <h2>Нет войне!</h2>
+const elem = React.createElement('h2', {className: 'greet'}, 'Hello!')
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  console.log(elem)
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
