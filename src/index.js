eadc4ed88146fc57d071578f7092ffde97dfd3ef
Ruslan Yuldashev <rustik.yuldashev@yandex.ru>
2022-06-02 22:33:36 +0300

    feat: сделать заготовку

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/app/app'
 import './index.css';
-import App from './App';
-
-
-// const elem = <h2>Нет войне!</h2>
-const elem = React.createElement('h2', {className: 'greet'}, 'Hello!')
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  console.log(elem)
+    <App />
 );
