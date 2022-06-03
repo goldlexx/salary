8daf1b21fd8c597a2217a8d9f6b63d1d1e249162
Ruslan Yuldashev <rustik.yuldashev@yandex.ru>
2022-06-03 22:12:37 +0300

    add: props and key

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,11 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
+  const data = [
+    {name: 'John S.', salary: 500, increase: false, id: 1},
+    {name: 'Vazgen S.', salary: 1500, increase: true, id: 2},
+    {name: 'Artur S.', salary: 5500, increase: false, id: 3}
+  ]
   return (
     <div className="app">
         <AppInfo />
@@ -16,7 +21,7 @@ function App() {
             <AppFilter/>
         </div>
         
-        <EmployeesList/>
+        <EmployeesList data={data}/>
         <EmployeesAddForm/>
     </div>
   );
