3fa7a30a079aa2000d843c500f3b0b741db247ac
Ruslan Yuldashev <rustik.yuldashev@yandex.ru>
2022-06-09 22:42:35 +0300

    feat: add filter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,25 +8,113 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
-  const data = [
-    {name: 'John S.', salary: 500, increase: false, id: 1},
-    {name: 'Vazgen S.', salary: 1500, increase: true, id: 2},
-    {name: 'Artur S.', salary: 5500, increase: false, id: 3}
-  ]
-  return (
-    <div className="app">
-        <AppInfo />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { name: 'John C.', salary: 800, increase: false, rise: true, id: 1 },
+        { name: 'Alex M.', salary: 3000, increase: true, rise: false, id: 2 },
+        { name: 'Carl W.', salary: 5000, increase: false, rise: false, id: 3 },
+      ],
+      term: '', filter: 'all'
 
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
+    };
+    this.maxId = 4;
+  }
+
+  deleteItem = (id) => {
+    this.setState(({ data }) => {
+      return {
+        data: data.filter((item) => item.id !== id),
+      };
+    });
+  };
+
+  // Да, пока могут добавляться пустые пользователи. Мы это еще исправим
+  addItem = (name, salary) => {
+    const newItem = {
+      name,
+      salary,
+      increase: false,
+      rise: false,
+      id: this.maxId++,
+    };
+    this.setState(({ data }) => {
+      const newArr = [...data, newItem];
+      return {
+        data: newArr,
+      };
+    });
+  };
+
+  onToggleProp = (id, prop) => {
+    this.setState(({data}) => ({
+      data: data.map(item => {
+        if (item.id === id) {
+          return {...item, [prop]: !item[prop]}
+        }
+        return item;
+      })
+    }))
+  };
+
+  searchEmp = (items, term) => {
+    if (term.length === 0) {
+      return items;
+    }
+
+    return items.filter(item => {
+      return item.name.indexOf(term) > -1
+    })
+  }
+
+  onUpadateSearch = (term) => {
+    this.setState({term});
+  }
+
+  filterPost = (items, filter) => {
+    switch (filter) {
+      case 'rise': 
+        return items.filter(item => item.rise === true);
+      case 'moreThen1000': 
+        return items.filter(item => item.salary > 1000);
+      default:
+        return items;  
+    }
+  }
+
+  onFilterSelect = (filter) => {
+    this.setState({filter});
+  }
+
+  render() {
+    const {data, term, filter} = this.state
+    const employees = this.state.data.length;
+    const increased = this.state.data.filter(item => item.increase).length;
+    const visibleData = this.filterPost(this.searchEmp(data, term), filter);
+
+    
+
+    
+    return (
+      <div className='app'>
+        <AppInfo employees={employees} increased={increased}/>
+
+        <div className='search-panel'>
+          <SearchPanel/>
+          <AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
         </div>
-        
-        <EmployeesList data={data}/>
-        <EmployeesAddForm/>
-    </div>
-  );
+
+        <EmployeesList
+          data={visibleData}
+          onDelete={this.deleteItem}
+          onToggleProp={this.onToggleProp}
+        />
+        <EmployeesAddForm onAdd={this.addItem} />
+      </div>
+    );
+  }
 }
 
 export default App;
